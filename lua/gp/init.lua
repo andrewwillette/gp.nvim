b7880f095a4dd5736477e8b4ef91636da27a6e1d
andrewwillette <willette.andrew1846@gmail.com>
2025-11-11 13:13:08 -0600

    fixing toggle_target usage with ChatNew

--- lua/gp/init.lua
+++ lua/gp/init.lua
@@ -829,12 +829,11 @@ M.cmd.ChatNew = function(params, system_prompt, agent)
 		return -1
 	end
 
-	-- if chat toggle is open, close it and start a new one
+	params.args = params.args or ""
+	if params.args == "" then
+		params.args = M.config.toggle_target
+	end
 	if M._toggle_close(M._toggle_kind.chat) then
-		params.args = params.args or ""
-		if params.args == "" then
-			params.args = M.config.toggle_target
-		end
 		return M.new_chat(params, true, system_prompt, agent)
 	end
 
